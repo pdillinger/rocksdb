307e87edf4532f25e5ad58081ced51e51278df1a
Peter Dillinger <peterd@fb.com>
2019-09-12 10:22:16 -0700

    Refactor DynamicBloom for compile-time implementation swap
    Summary: Refactored DynamicBloom to split between some common
    functionality and implementation details that can be swapped out at
    compile time. (Expecting another implementation soon.)
    
    Test Plan: existing dynamic bloom unit test (and others). No consistent
    performance improvement or regression seen before vs. after with
      ./dynamic_bloom_test --enable_perf

--- util/dynamic_bloom.cc
+++ util/dynamic_bloom.cc
@@ -5,67 +5,32 @@
 
 #include "dynamic_bloom.h"
 
-#include <algorithm>
-
-#include "memory/allocator.h"
-#include "port/port.h"
-#include "rocksdb/slice.h"
-#include "util/hash.h"
-
 namespace rocksdb {
 
-namespace {
-
-uint32_t roundUpToPow2(uint32_t x) {
-  uint32_t rv = 1;
-  while (rv < x) {
-    rv <<= 1;
-  }
-  return rv;
-}
-
-}
-
-DynamicBloom::DynamicBloom(Allocator* allocator, uint32_t total_bits,
-                           uint32_t num_probes,
-                           size_t huge_page_tlb_size, Logger* logger)
-    // Round down, except round up with 1
-    : kNumDoubleProbes((num_probes + (num_probes == 1)) / 2) {
+void DynamicBloomImpl::SetNumProbes(uint32_t num_probes) {
   assert(num_probes % 2 == 0); // limitation of current implementation
   assert(num_probes <= 10); // limitation of current implementation
-  assert(kNumDoubleProbes > 0);
+  num_double_probes_ = (num_probes + (num_probes == 1)) / 2;
+  assert(num_double_probes_ > 0);
+  assert(num_double_probes_ <= 5);
+}
 
-  // Determine how much to round off + align by so that x ^ i (that's xor) is
-  // a valid u64 index if x is a valid u64 index and 0 <= i < kNumDoubleProbes.
-  uint32_t block_bytes = /*bytes/u64*/ 8 *
-                         /*u64s*/ std::max(1U, roundUpToPow2(kNumDoubleProbes));
-  uint32_t block_bits = block_bytes * 8;
-  uint32_t blocks = (total_bits + block_bits - 1) / block_bits;
-  uint32_t sz = blocks * block_bytes;
-  kLen = sz / /*bytes/u64*/8;
-  assert(kLen > 0);
-#ifndef NDEBUG
-  for (uint32_t i = 0; i < kNumDoubleProbes; ++i) {
-    // Ensure probes starting at last word are in range
-    assert(((kLen - 1) ^ i) < kLen);
-  }
-#endif
+uint32_t DynamicBloomImpl::GetBlockBytes() {
+  uint32_t nextPowTwo = 1;
+  while (nextPowTwo < num_double_probes_) { nextPowTwo <<= 1; }
+  // How much to round off + align by so that x ^ i (that's xor) is a valid
+  // u64 index if x is a valid u64 index and 0 <= i < num_double_probes_.
+  return /*bytes/u64*/ 8 * /*u64s*/nextPowTwo;
+}
 
-  // Padding to correct for allocation not originally aligned on block_bytes
-  // boundary
-  sz += block_bytes - 1;
-  assert(allocator);
-
-  char* raw = allocator->AllocateAligned(sz, huge_page_tlb_size, logger);
-  memset(raw, 0, sz);
-  auto block_offset = reinterpret_cast<uintptr_t>(raw) % block_bytes;
-  if (block_offset > 0) {
-    // Align on block_bytes boundary
-    raw += block_bytes - block_offset;
-  }
+void DynamicBloomImpl::SetData(char *data, size_t size) {
   static_assert(sizeof(std::atomic<uint64_t>) == sizeof(uint64_t),
                 "Expecting zero-space-overhead atomic");
-  data_ = reinterpret_cast<std::atomic<uint64_t>*>(raw);
+  data_ = reinterpret_cast<std::atomic<uint64_t>*>(data);
+  // Ensure aligned size and pointer
+  assert(size % GetBlockBytes() == 0);
+  assert(reinterpret_cast<uintptr_t>(data) % GetBlockBytes() == 0);
+  len_ = size / /*bytes/u64*/8;
 }
 
 }  // rocksdb
--- util/dynamic_bloom.h
+++ util/dynamic_bloom.h
@@ -9,6 +9,7 @@
 
 #include "rocksdb/slice.h"
 
+#include "memory/allocator.h"
 #include "port/port.h"
 #include "util/hash.h"
 
@@ -20,6 +21,62 @@ namespace rocksdb {
 class Slice;
 class Allocator;
 class Logger;
+
+
+// A Bloom filter intended only to be used in memory, never serialized in a way
+// that could lead to schema incompatibility. Supports opt-in lock-free
+// concurrent access.
+//
+// Implementation details are compile-time swappable to make DynamicBloom
+// type below.
+//
+template <class Impl>
+class DynamicBloomTemplate : public Impl {
+ public:
+  // allocator: pass allocator to bloom filter, hence trace the usage of memory
+  // total_bits: fixed total bits for the bloom
+  // num_probes: number of hash probes for a single key
+  // hash_func:  customized hash function
+  // huge_page_tlb_size:  if >0, try to allocate bloom bytes from huge page TLB
+  //                      within this page size. Need to reserve huge pages for
+  //                      it to be allocated, like:
+  //                         sysctl -w vm.nr_hugepages=20
+  //                     See linux doc Documentation/vm/hugetlbpage.txt
+  explicit DynamicBloomTemplate(Allocator* allocator,
+                                uint32_t total_bits,
+                                uint32_t num_probes = 6,
+                                size_t huge_page_tlb_size = 0,
+                                Logger* logger = nullptr);
+
+  // Assuming single threaded access to this function.
+  void Add(const Slice& key);
+
+  // Like Add, but may be called concurrent with other functions.
+  void AddConcurrently(const Slice& key);
+
+  // Multithreaded access to this function is OK
+  bool MayContain(const Slice& key) const;
+
+// Inherited public:
+  // Assuming single threaded access to this function.
+  //void AddHash(uint32_t hash);
+
+  // Like AddHash, but may be called concurrent with other functions.
+  //void AddHashConcurrently(uint32_t hash);
+
+  // Multithreaded access to this function is OK
+  //bool MayContainHash(uint32_t hash) const;
+
+  // Multithreaded access to this function is OK
+  //void Prefetch(uint32_t hash);
+
+// Inherited protected:
+  //void SetNumProbes(uint32_t num_probes);
+
+  //uint32_t GetBlockBytes();
+
+  //void SetData(char *data, size_t size);
+};
 
 // A Bloom filter intended only to be used in memory, never serialized in a way
 // that could lead to schema incompatibility. Supports opt-in lock-free
@@ -33,104 +90,6 @@ class Logger;
 // For simplicity and performance, the current implementation requires
 // num_probes to be a multiple of two and <= 10.
 //
-class DynamicBloom {
- public:
-  // allocator: pass allocator to bloom filter, hence trace the usage of memory
-  // total_bits: fixed total bits for the bloom
-  // num_probes: number of hash probes for a single key
-  // hash_func:  customized hash function
-  // huge_page_tlb_size:  if >0, try to allocate bloom bytes from huge page TLB
-  //                      within this page size. Need to reserve huge pages for
-  //                      it to be allocated, like:
-  //                         sysctl -w vm.nr_hugepages=20
-  //                     See linux doc Documentation/vm/hugetlbpage.txt
-  explicit DynamicBloom(Allocator* allocator,
-                        uint32_t total_bits,
-                        uint32_t num_probes = 6,
-                        size_t huge_page_tlb_size = 0,
-                        Logger* logger = nullptr);
-
-  ~DynamicBloom() {}
-
-  // Assuming single threaded access to this function.
-  void Add(const Slice& key);
-
-  // Like Add, but may be called concurrent with other functions.
-  void AddConcurrently(const Slice& key);
-
-  // Assuming single threaded access to this function.
-  void AddHash(uint32_t hash);
-
-  // Like AddHash, but may be called concurrent with other functions.
-  void AddHashConcurrently(uint32_t hash);
-
-  // Multithreaded access to this function is OK
-  bool MayContain(const Slice& key) const;
-
-  // Multithreaded access to this function is OK
-  bool MayContainHash(uint32_t hash) const;
-
-  void Prefetch(uint32_t h);
-
- private:
-  // Length of the structure, in 64-bit words. For this structure, "word"
-  // will always refer to 64-bit words.
-  uint32_t kLen;
-  // We make the k probes in pairs, two for each 64-bit read/write. Thus,
-  // this stores k/2, the number of words to double-probe.
-  const uint32_t kNumDoubleProbes;
-
-  std::atomic<uint64_t>* data_;
-
-  // or_func(ptr, mask) should effect *ptr |= mask with the appropriate
-  // concurrency safety, working with bytes.
-  template <typename OrFunc>
-  void AddHash(uint32_t hash, const OrFunc& or_func);
-};
-
-inline void DynamicBloom::Add(const Slice& key) { AddHash(BloomHash(key)); }
-
-inline void DynamicBloom::AddConcurrently(const Slice& key) {
-  AddHashConcurrently(BloomHash(key));
-}
-
-inline void DynamicBloom::AddHash(uint32_t hash) {
-  AddHash(hash, [](std::atomic<uint64_t>* ptr, uint64_t mask) {
-    ptr->store(ptr->load(std::memory_order_relaxed) | mask,
-               std::memory_order_relaxed);
-  });
-}
-
-inline void DynamicBloom::AddHashConcurrently(uint32_t hash) {
-  AddHash(hash, [](std::atomic<uint64_t>* ptr, uint64_t mask) {
-    // Happens-before between AddHash and MaybeContains is handled by
-    // access to versions_->LastSequence(), so all we have to do here is
-    // avoid races (so we don't give the compiler a license to mess up
-    // our code) and not lose bits.  std::memory_order_relaxed is enough
-    // for that.
-    if ((mask & ptr->load(std::memory_order_relaxed)) != mask) {
-      ptr->fetch_or(mask, std::memory_order_relaxed);
-    }
-  });
-}
-
-inline bool DynamicBloom::MayContain(const Slice& key) const {
-  return (MayContainHash(BloomHash(key)));
-}
-
-#if defined(_MSC_VER)
-#pragma warning(push)
-// local variable is initialized but not referenced
-#pragma warning(disable : 4189)
-#endif
-inline void DynamicBloom::Prefetch(uint32_t h32) {
-  size_t a = fastrange32(kLen, h32);
-  PREFETCH(data_ + a, 0, 3);
-}
-#if defined(_MSC_VER)
-#pragma warning(pop)
-#endif
-
 // Speed hacks in this implementation:
 // * Uses fastrange instead of %
 // * Minimum logic to determine first (and all) probed memory addresses.
@@ -151,9 +110,130 @@ inline void DynamicBloom::Prefetch(uint32_t h32) {
 // NB: could easily be upgraded to support a 64-bit hash and
 // total_bits > 2^32 (512MB). (The latter is a bad idea without the former,
 // because of false positives.)
+//
+class DynamicBloomImpl {
+public:
+  DynamicBloomImpl() : num_double_probes_(0), len_(0), data_(nullptr) {}
+  ~DynamicBloomImpl() {}
 
-inline bool DynamicBloom::MayContainHash(uint32_t h32) const {
-  size_t a = fastrange32(kLen, h32);
+  void Prefetch(uint32_t hash);
+
+  bool MayContainHash(uint32_t hash) const;
+
+  void AddHash(uint32_t hash);
+
+  void AddHashConcurrently(uint32_t hash);
+
+protected:
+  void SetNumProbes(uint32_t num_probes);
+
+  uint32_t GetBlockBytes();
+
+  void SetData(char *data, size_t size);
+
+ private:
+  // We make the k probes in pairs, two for each 64-bit read/write. Thus,
+  // this stores k/2, the number of words to double-probe.
+  uint32_t num_double_probes_;
+
+  // Length of the structure, in 64-bit words. For this structure, "word"
+  // will always refer to 64-bit words.
+  uint32_t len_;
+
+  // Raw filter data
+  std::atomic<uint64_t>* data_;
+
+  // or_func(ptr, mask) should effect *ptr |= mask with the appropriate
+  // concurrency safety.
+  template <typename OrFunc>
+  inline void AddHash(uint32_t hash, const OrFunc& or_func);
+};
+
+
+typedef DynamicBloomTemplate<DynamicBloomImpl> DynamicBloom;
+
+
+// Implementation details that are templated or should be inlined:
+
+template <class Impl>
+DynamicBloomTemplate<Impl>::DynamicBloomTemplate(Allocator* allocator,
+                                           uint32_t total_bits,
+                                           uint32_t num_probes,
+                                           size_t huge_page_tlb_size,
+                                           Logger* logger) {
+  Impl::SetNumProbes(num_probes);
+  // Determine how much to round off + align by
+  uint32_t block_bytes = Impl::GetBlockBytes();
+  uint32_t block_bits = block_bytes * 8;
+  uint32_t blocks = (total_bits + block_bits - 1) / block_bits;
+  uint32_t sz = blocks * block_bytes;
+
+  // Padding to correct for allocation not originally aligned on block_bytes
+  // boundary
+  sz += block_bytes - 1;
+  assert(allocator);
+
+  char* raw = allocator->AllocateAligned(sz, huge_page_tlb_size, logger);
+  memset(raw, 0, sz);
+  auto block_offset = reinterpret_cast<uintptr_t>(raw) % block_bytes;
+  if (block_offset > 0) {
+    // Align on block_bytes boundary
+    raw += block_bytes - block_offset;
+  }
+  Impl::SetData(raw, blocks * block_bytes);
+}
+
+template <class Impl>
+inline void DynamicBloomTemplate<Impl>::Add(const Slice& key) {
+  Impl::AddHash(BloomHash(key));
+}
+
+template <class Impl>
+inline void DynamicBloomTemplate<Impl>::AddConcurrently(const Slice& key) {
+  Impl::AddHashConcurrently(BloomHash(key));
+}
+
+template <class Impl>
+inline bool DynamicBloomTemplate<Impl>::MayContain(const Slice& key) const {
+  return Impl::MayContainHash(BloomHash(key));
+}
+
+
+inline void DynamicBloomImpl::AddHash(uint32_t hash) {
+  AddHash(hash, [](std::atomic<uint64_t>* ptr, uint64_t mask) {
+    ptr->store(ptr->load(std::memory_order_relaxed) | mask,
+               std::memory_order_relaxed);
+  });
+}
+
+inline void DynamicBloomImpl::AddHashConcurrently(uint32_t hash) {
+  AddHash(hash, [](std::atomic<uint64_t>* ptr, uint64_t mask) {
+    // Happens-before between AddHash and MaybeContains is handled by
+    // access to versions_->LastSequence(), so all we have to do here is
+    // avoid races (so we don't give the compiler a license to mess up
+    // our code) and not lose bits.  std::memory_order_relaxed is enough
+    // for that.
+    if ((mask & ptr->load(std::memory_order_relaxed)) != mask) {
+      ptr->fetch_or(mask, std::memory_order_relaxed);
+    }
+  });
+}
+
+#if defined(_MSC_VER)
+#pragma warning(push)
+// local variable is initialized but not referenced
+#pragma warning(disable : 4189)
+#endif
+inline void DynamicBloomImpl::Prefetch(uint32_t h32) {
+  size_t a = fastrange32(len_, h32);
+  PREFETCH(data_ + a, 0, 3);
+}
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
+
+inline bool DynamicBloomImpl::MayContainHash(uint32_t h32) const {
+  size_t a = fastrange32(len_, h32);
   PREFETCH(data_ + a, 0, 3);
   // Expand/remix with 64-bit golden ratio
   uint64_t h = 0x9e3779b97f4a7c13ULL * h32;
@@ -162,7 +242,7 @@ inline bool DynamicBloom::MayContainHash(uint32_t h32) const {
     uint64_t mask = ((uint64_t)1 << (h & 63))
                   | ((uint64_t)1 << ((h >> 6) & 63));
     uint64_t val = data_[a ^ i].load(std::memory_order_relaxed);
-    if (i + 1 >= kNumDoubleProbes) {
+    if (i + 1 >= num_double_probes_) {
       return (val & mask) == mask;
     } else if ((val & mask) != mask) {
       return false;
@@ -172,8 +252,8 @@ inline bool DynamicBloom::MayContainHash(uint32_t h32) const {
 }
 
 template <typename OrFunc>
-inline void DynamicBloom::AddHash(uint32_t h32, const OrFunc& or_func) {
-  size_t a = fastrange32(kLen, h32);
+inline void DynamicBloomImpl::AddHash(uint32_t h32, const OrFunc& or_func) {
+  size_t a = fastrange32(len_, h32);
   PREFETCH(data_ + a, 0, 3);
   // Expand/remix with 64-bit golden ratio
   uint64_t h = 0x9e3779b97f4a7c13ULL * h32;
@@ -182,7 +262,7 @@ inline void DynamicBloom::AddHash(uint32_t h32, const OrFunc& or_func) {
     uint64_t mask = ((uint64_t)1 << (h & 63))
                   | ((uint64_t)1 << ((h >> 6) & 63));
     or_func(&data_[a ^ i], mask);
-    if (i + 1 >= kNumDoubleProbes) {
+    if (i + 1 >= num_double_probes_) {
       return;
     }
     h = (h >> 12) | (h << 52);
